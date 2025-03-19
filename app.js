a381fde9b4701e131d95fa81e97db9a5091915da
2023154 <2023154@student.cct.ie>
2025-03-19 23:38:26 +0000

    fixing code.
    Renan and I were both working on the same line of code, and when we tried to push our changes, we ran into a bug. One of us couldn’t push the commit, probably because of a conflict between our edits. It caused a bit of a mess, but after sorting things out, the code is all fixed now, and everything is back in place.

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const prevMusic = document.getElementById("prevMusic");
 const nextMusic = document.getElementById("nextMusic");
 const pauseMusic = document.getElementById("pauseMusic");
 const trackTitle = document.getElementById("trackTitle");
-
-const themeToggle = document.getElementById("themeToggle");
-const menuIcon = document.getElementById("menuIcon");
-
-
 const albumPhoto = document.getElementById("albumPhoto");
+// const themeToggle = document.getElementById("themeToggle");
+// const menuIcon = document.getElementById("menuIcon");
+//
+
+
 const playlist = [
     {title: "1 cuff-it-wetter 1",  src:"music/cuff-it-wetter.mp3", img:"music/img.png"},
     {title: "song 2", src:"...", img: ""}, // need more songss.
@@ -49,26 +49,7 @@ pauseMusic.addEventListener("click", () => {
     }
 });
 
-if (localStorage.geItem("theme") === "light") {
-    document.body.classList.add("light-mode");
-    themeToggle.textContent = "🌞";
 
-}
-themeToggle.addEventListener("click", () => {
-        document.body.classList.remove("light-mode");
-
-        if (ddocument.body.classList.contains("light-mode")) {
-            localStorage.setItem("theme", "light");
-            themeToggle.textContent = "🔆";
-        } else {
-            localStorage.setItem("theme", "dark");
-            themeToggle.textContent = "🌑";
-        }
-    });
-
-    menuIcon.addEventListener("click", () => {
-        alert("lateral menu")
-    });
 
 
 //next music
@@ -108,3 +89,24 @@ volumeSlider.addEventListener("input", () => {
 audio.addEventListener("ended", () =>{
     nextMusic.click();
 })
+
+if (localStorage.geItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+    themeToggle.textContent = "🌞";
+
+}
+themeToggle.addEventListener("click", () => {
+    document.body.classList.remove("light-mode");
+
+    if (ddocument.body.classList.contains("light-mode")) {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "🔆";
+    } else {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "🌑";
+    }
+});
+
+menuIcon.addEventListener("click", () => {
+    alert("lateral menu")
+});
