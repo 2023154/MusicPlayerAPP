ed98220311a3b925b46ce0407bdf159d0541ea84
LucasOhenes <2023302@student.cct.ie>
2025-03-23 21:50:43 +0000

    Creating the main.js with electron import and window specifications

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js') // Ensure 'preload.js' exists
+    }
+  });
+
+  win.loadFile('app.html');
+};
+
+// When Electron is ready, create the window
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
